e6cbb00006ce6ec4dcc491dd747a3ab0e4efa7e9
Pablo Giaccon <psjacon1995@gmail.com>
2020-06-16 08:26:59 -0300

    Correcting Errors

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
 import { View } from 'react-native';
-
-import FloatingCart from '../../components/FloatingCart';
 
 import {
   Container,
@@ -20,6 +18,9 @@ import {
   ProductQuantity,
   ActionContainer,
   ActionButton,
+  TotalProductsContainer,
+  TotalProductsText,
+  SubtotalValue,
 } from './styles';
 
 import { useCart } from '../../hooks/cart';
@@ -44,6 +45,24 @@ const Cart: React.FC = () => {
   function handleDecrement(id: string): void {
     decrement(id);
   }
+
+  const cartTotal = useMemo(() => {
+    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const totalProduct = product.price * product.quantity;
+      return accumulator + totalProduct;
+    }, 0);
+    return formatValue(total);
+  }, [products]);
+
+  const totalItensInCart = useMemo(() => {
+    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const quantityProduct = product.quantity;
+      return accumulator + quantityProduct;
+    }, 0);
+    return total;
+  }, [products]);
 
   return (
     <Container>
@@ -92,7 +111,11 @@ const Cart: React.FC = () => {
           )}
         />
       </ProductContainer>
-      <FloatingCart />
+      <TotalProductsContainer>
+        <FeatherIcon name="shopping-cart" color="#fff" size={24} />
+        <TotalProductsText>{`${totalItensInCart} itens`}</TotalProductsText>
+        <SubtotalValue>{cartTotal}</SubtotalValue>
+      </TotalProductsContainer>
     </Container>
   );
 };
